60a03990bd712c9a344fe491ac38236e6cd14257
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-10-14 19:04:17 -0300

    Show an dialog when has not Personal Access Token

--- bitrise/slack-app/functions/commands/bitrise.js
+++ bitrise/slack-app/functions/commands/bitrise.js
@@ -1,5 +1,6 @@
 const lib = require('lib')({token: process.env.STDLIB_TOKEN});
 const request = require('request');
+const storage = require('../../helpers/storage.js');
 
 /**
 * /test
@@ -18,62 +19,89 @@ const request = require('request');
 * @returns {object}
 */
 module.exports = (user, channel, text = 'master', command = {}, botToken = null, callback) => {
-  if (text.trim().length == 0) {
-    text = 'master';
-  }
-
-  const options = {
-    url: 'https://api.bitrise.io/v0.1/apps?sort_by=last_build_at',
-    method: 'GET',
-    headers: {
-      Authorization: 'token ***REMOVED***'
-    },
-    json: true
-  };
-
-  request.get(options, function(err, res, body) {
-    var message = 'Send cURL request to Bitrise!';
-    var attachments = [];
-
-    if (body.error_msg != undefined) {
-      message = body.error_msg;
+  storage.getBitriseAppToken(command.teamId, (value) => {
+    if (text.trim().length == 0) {
+      text = 'master';
     }
 
-    if (body.status == 'error' && body.message != undefined) {
-      message = body.message;
+    let setBitriseAppTokenDialog = {
+      dialog: {
+        callback_id: 'access_token',
+        title: 'Personal Access Token',
+        submit_label: 'Update',
+        notify_on_cancel: true,
+        elements: [
+          {
+            type: 'text',
+            label: 'THE-ACCESS-TOKEN',
+            placeholder: 'Tokens that can be used to access the Bitrise API.',
+            name: 'token',
+            min_length: 80,
+            hint: 'Generate through https://app.bitrise.io/me/profile#/security'
+          }
+        ]
+      }
+    };
+
+    if (!value) {
+      callback(null, setBitriseAppTokenDialog);
+
+      return;
     }
 
-    var actions = [];
+    const options = {
+      url: 'https://api.bitrise.io/v0.1/apps?sort_by=last_build_at',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${value}`
+      },
+      json: true
+    };
 
-    if (body.data != undefined) {
-      actions = body.data.map(app => { 
-        return {
-          name: 'app',
-          text: app.title,
-          type: 'button',
-          value: JSON.stringify({ branch: text, app_slug: app.slug })
-        }
+    request.get(options, function (err, res, body) {
+      var message = 'Send cURL request to Bitrise!';
+      var attachments = [];
+
+      if (body.error_msg != undefined) {
+        message = body.error_msg;
+      }
+
+      if (body.status == 'error' && body.message != undefined) {
+        message = body.message;
+      }
+
+      var actions = [];
+
+      if (body.data != undefined) {
+        actions = body.data.map(app => {
+          return {
+            name: 'app',
+            text: app.title,
+            type: 'button',
+            value: JSON.stringify({ branch: text, app_slug: app.slug })
+          }
+        });
+      }
+
+      if (actions.length == 0) {
+        return callback(null, {
+          'text': message,
+          'attachments': attachments
+        })
+      }
+
+      callback(null, {
+        "text": "Which app will you run now?",
+        "attachments": [
+          {
+            "fallback": "You are unable to choose an app",
+            "callback_id": "app_id",
+            "color": "#492f5c",
+            "attachment_type": "default",
+            "actions": actions
+          }
+        ]
       });
-    }
-
-    if (actions.length == 0) {
-      return callback(null, {
-        'text': message,
-        'attachments': attachments
-      })
-    }
-
-    callback(null, {
-      "text": "Which app will you run now?",
-      "attachments": [
-        {
-          "fallback": "You are unable to choose an app",
-          "callback_id": "app_id",
-          "color": "#492f5c",
-          "attachment_type": "default",
-          "actions": actions
-        }
-      ]
     });
   });
 };
